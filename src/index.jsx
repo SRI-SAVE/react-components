fa5b4051210971468db44a8c5c732697f9027b3f
John Pywtorak <jpywtora@gmail.com>
2015-10-14 17:44:04 -0700

    Fixes to the style for the body appended element, correcting it since it is true css so I need the px

--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,7 @@ import { EUI } from './components/EUI';
 
 const elemDiv = document.createElement('div');
 
-elemDiv.style.cssText = 'position:fixed;top:5;left:5;';
+elemDiv.style.cssText = 'position:fixed;top:5px;left:5px;';
+elemDiv.id = 'save-react-components';
 React.render(<EUI/>, elemDiv);
 document.body.appendChild(elemDiv);
