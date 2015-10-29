87396bd2a640d12026ade031340b292ee2ec1c08
John Pywtorak <jpywtora@gmail.com>
2015-10-28 17:31:23 -0700

    Fix for host not being correct

--- src/sandbox.js
+++ src/sandbox.js
@@ -2,17 +2,18 @@
 /*global __WEBPACK_DEV_SERVER_NO_FF__*/
 
 let SAVE;
+let host = window.location.hostname;
 
 if (__WEBPACK_DEV_SERVER_DEBUG__) { // for webpack-dev-server auto simulate
   SAVE = require('./webpack-dev-server-util');
-  
+
   if (!__WEBPACK_DEV_SERVER_NO_FF__) require('./webpack-dev-server-fetch');
 }
 
 window.SAVE2 = {
   VERSION: '2.0.1',
   lib: { view: SAVE },
-  isCAT: () => window._EntityLibrary != null || window.location.hostname === 'save.github.io',
+  isCAT: () => window._EntityLibrary != null || host === 'sri-save.github.io' || host === '',
   simulate: () => {
     window.SAVE2.lib.view = require('./webpack-dev-server-util');
     require('./webpack-dev-server-fetch');
