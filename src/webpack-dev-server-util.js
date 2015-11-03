1d5d59a584906573e71d26d6b3b4e97ff28b3e1b
John Pywtorak <jpywtora@gmail.com>
2015-11-03 10:40:01 -0800

    Changed the logging of api calls to lib.view of the Sandbox

--- src/webpack-dev-server-util.js
+++ src/webpack-dev-server-util.js
@@ -3,17 +3,22 @@ if (__WEBPACK_SAVE_MODE__ === 'CAT') window._EntityLibrary = { };
 
 export default  {
   createSemanticAsset() {
-    console.info('createSemanticAsset()');
-    console.info(arguments);
+    console.group('SAVE2.lib.view.createSemanticAsset()');
+      console.table([ arguments ]);
+      // console.dir(arguments);
+    console.groupEnd();
   },
 
   installAutoLoads() {
-    console.info('installAutoLoads');
-    console.info(arguments);
+    console.group('SAVE2.lib.view.installAutoLoads()');
+      console.table([ arguments ]);
+    console.groupEnd();
   },
 
   setBaseServerAddress() {
-    console.info('setBaseServerAddress');
-    console.info(arguments);
+    console.group('SAVE2.lib.view.setBaseServerAddress()');
+      console.table([ arguments ]);
+      // console.dir(arguments);
+    console.groupEnd();
   },
 };
